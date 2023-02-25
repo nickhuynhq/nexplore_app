4e9be1aa4bba51b4597ccf7e9422ef15b26f7cbc
Nicholas Huynh <nickhuynhq@gmail.com>
2023-02-25 15:25:10 -0500

    Change to SafeAreaView for web

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
 
 export default function App() {
   return (
     <TailwindProvider>
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
         <Text>Open up App.tsx to start working on your app!</Text>
         <StatusBar style="auto" />
-      </View>
+      </SafeAreaView>
     </TailwindProvider>
   );
 }
