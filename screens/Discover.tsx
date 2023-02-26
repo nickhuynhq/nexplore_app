15c97cc68628e47d9af7f3ed5e0d7a48c0cb5ac5
Nicholas Huynh <nickhuynhq@gmail.com>
2023-02-26 10:29:08 -0500

    Add top bar for Discover screen

--- screens/Discover.tsx
+++ screens/Discover.tsx
@@ -1,8 +1,9 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, Image, SafeAreaView } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../App";
 import { useNavigation } from "@react-navigation/native";
+import { Avatar } from "../assets";
 
 type discoverProps = StackNavigationProp<RootStackParamList, "Discover">;
 
@@ -17,10 +18,18 @@ const Discover = () => {
 
   return (
     <SafeAreaView className="flex-1 bg-slate-50 relative">
-      <View className="flex-row items-center justify-between px-8">
-      <Text>Discover</Text>
-      </View>
+      {/* Top Bar */}
+      <View className="flex-row items-center justify-between px-8 mt-8">
+        <Text className="text-4xl font-bold">Discover</Text>
 
+        {/* Profile Picture Container */}
+        <View className="w-12 h-12 bg-gray-400 shadow-md rounded-md items-center justify-center">
+          <Image
+            source={Avatar}
+            className="w-full h-full rounded-md object-cover"
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
