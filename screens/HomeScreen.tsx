7dc330189f46e52b81f083f845aa7df7422e21dc
Nicholas Huynh <nickhuynhq@gmail.com>
2023-02-25 21:03:51 -0500

    Redo HomeScreen styling and removed image of man

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -9,10 +9,10 @@ import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { HeroImage, Logo } from "../assets";
 import * as Animatable from "react-native-animatable";
-import {StackNavigationProp} from '@react-navigation/stack';
+import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../App";
 
-type homeScreenProp = StackNavigationProp<RootStackParamList, 'Home'>;
+type homeScreenProp = StackNavigationProp<RootStackParamList, "Home">;
 
 const HomeScreen = () => {
   // returns the navigation prop of the screen it's inside.
@@ -28,74 +28,66 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView className="flex h-full w-full bg-slate-50">
-      <View className="h-[33vh]">
-        {/* Top Section */}
-        <View className="flex flex-row w-full px-6 pt-6">
-          <Animatable.Image
-            animation="bounceInLeft"
-            easing="ease-in-out"
-            source={Logo}
-            resizeMode="contain"
-            className="w-2/3 h-14"
-          />
-        </View>
-
-        {/* Hero Section */}
-        <View className="px-6 mt-8 space-y-3">
-          <Animatable.Text
-            animation="fadeInLeft"
-            delay={1000}
-            className="text-slate-600 text-[42px]"
-          >
-            Get out there and discover your
-          </Animatable.Text>
-          <Animatable.Text
-            animation="fadeInLeft"
-            delay={1500}
-            className="text-black font-bold text-[38px]"
-          >
-            Next Adventure!
-          </Animatable.Text>
-        </View>
+    <SafeAreaView className="flex h-full w-full bg-slate-50 relative">
+        
+      <View className="abolute z-0 w-full h-[100vh]">
+        <Animatable.Image
+          animation="fadeInUp"
+          easing="ease-in-out"
+          source={{ uri: "https://i.pinimg.com/originals/b7/72/b3/b772b392f3a3326d75d33eaa531270b5.jpg" }}
+          className=" w-full h-full "
+        />
       </View>
 
-      <View className="flex items-center justify-center h-full w-full relative">
-        {/* Background circles */}
-        <View className="h-full w-full">
-          <Animatable.View
-            animation="fadeInUp"
-            delay={400}
-            className="w-[320px] h-[320px] bg-sky-500 rounded-full absolute -right-32"
-          ></Animatable.View>
-          <Animatable.View
-            animation="fadeInUp"
-            delay={400}
-            className="w-[320px] h-[320px] bg-orange-400 rounded-full absolute  bottom-60 -left-12"
-          ></Animatable.View>
+      <View className=" absolute bg-slate-50 w-[100vw] h-[100vh] opacity-60"></View>
 
-          <Animatable.Image
-            animation="fadeInUp"
-            easing="ease-in-out"
-            source={HeroImage}
-            className="w-full h-[80%]"
-          />
+      <View className="absolute w-full h-full py-16 z-10">
+        <View className="h-[33vh]">
+          {/* Top Section */}
+          <View className="flex flex-row w-full px-6 pt-6">
+            <Animatable.Image
+              animation="bounceInLeft"
+              easing="ease-in-out"
+              source={Logo}
+              resizeMode="contain"
+              className="w-2/3 h-14"
+            />
+          </View>
+
+          {/* Hero Section */}
+          <View className="px-6 mt-8 space-y-3">
+            <Animatable.Text
+              animation="fadeInLeft"
+              delay={1000}
+              className="text-slate-900 text-[42px]"
+            >
+              Get out there and discover your
+            </Animatable.Text>
+            <Animatable.Text
+              animation="fadeInLeft"
+              delay={1500}
+              className="text-black font-bold text-[38px]"
+            >
+              Next Adventure!
+            </Animatable.Text>
+          </View>
         </View>
 
-        {/* Go Button */}
-        <View className="flex items-center justify-center absolute w-32 h-32 border-sky-500 border-4 rounded-full bg-transparent">
-          <TouchableOpacity
-          onPress={() => navigation.navigate("Discover")}
-          >
-            <Animatable.View
-              animation={"pulse"}
-              easing="ease-in-out"
-              iterationCount={"infinite"}
-              className="flex items-center justify-center bg-sky-500 w-28 h-28 rounded-full"
-            >
-              <Text className="text-white text-3xl font-bold">Start</Text>
-            </Animatable.View>
-          </TouchableOpacity>
+        <View className="flex items-center justify-center h-full w-full relative">
+
+          {/* Go Button */}
+          <View className="flex items-center justify-center absolute w-32 h-32 border-black border-4 rounded-full bg-transparent">
+            <TouchableOpacity onPress={() => navigation.navigate("Discover")}>
+              <Animatable.View
+                animation={"pulse"}
+                easing="ease-in-out"
+                iterationCount={"infinite"}
+                className="flex items-center justify-center bg-black w-28 h-28 rounded-full"
+              >
+                <Text className="text-white text-3xl font-bold">Start</Text>
+              </Animatable.View>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </SafeAreaView>
