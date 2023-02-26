17b45f853bf806a292443dd5744c65b28febb374
Nicholas Huynh <nickhuynhq@gmail.com>
2023-02-25 20:49:45 -0500

    Use SafeAreaView for Discover

--- screens/Discover.tsx
+++ screens/Discover.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../App";
@@ -16,9 +16,9 @@ const Discover = () => {
   }, []);
 
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-slate-50 relative">
       <Text>Discover</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
